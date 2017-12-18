ce1c015c6231213ba3e47dd6ebe714eb1a9a74df
devon96 <kdryja@gmail.com>
2017-12-18 18:01:28 +0100

    Minor changes

--- Controller.py
+++ Controller.py
@@ -5,25 +5,20 @@ import SimpleMFRC522
 OPEN = 5
 CLOSED = 10
 
+
 class Lock:
     def __init__(self):
         self.status = 1
-        self.init_gpio()
-
-    def init_gpio(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(18,GPIO.OUT)
-        self.p = GPIO.PWM(18,50)
+        GPIO.setup(18, GPIO.OUT)
+        self.p = GPIO.PWM(18, 50)
         self.p.start(5)
         time.sleep(1)
 
     def change_lock_position(self):
-        self.init_gpio()
-        time.sleep(1)
-
         duty = 0
-        if self.status==1:
+        if self.status == 1:
             duty = 10
             self.status = 0
         else:
@@ -32,28 +27,27 @@ class Lock:
 
         self.p.ChangeDutyCycle(duty)
         time.sleep(1)
-        GPIO.cleanup()
-        time.sleep(0.5)
+
 
 class NFCReader:
     def __init__(self):
         self.reader = SimpleMFRC522.SimpleMFRC522()
 
     def read(self):
-        try:
-            id, text = self.reader.read()
-            print(id)
-            print(text)
-        finally:
-            GPIO.cleanup()
-            return text
+        text = self.reader.read()[1]
+        return text
+
 
 lock = Lock()
 reader = NFCReader()
 
 while True:
-    card_content = reader.read().strip()
-    if card_content=="Open!":
-        lock.change_lock_position()
-    else:
-        print "WRONG CARD!\n"
+    try:
+        card_content = reader.read().strip()
+        if card_content == "Open!":
+            lock.change_lock_position()
+            print "Card accepted!\n"
+        else:
+            print "WRONG CARD!\n"
+    except KeyboardInterrupt:
+        GPIO.cleanup()
